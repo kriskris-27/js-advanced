7af6e5a6246a428584d1a30bddbfc9ef8b6cbeb0
Krishnakumar <krishnakumars0101@gmail.com>
2024-09-13 10:48:30 +0530

    project file added to todo

---
+++ todo/project.js
@@ -0,0 +1,18 @@
+// project.js
+export function createProject(name) {
+    const todos = [];
+
+    return {
+        name,
+        todos,
+        addTodo(todo) {
+            this.todos.push(todo);
+        },
+        removeTodo(todoTitle) {
+            this.todos = this.todos.filter(todo => todo.title !== todoTitle);
+        },
+        getTodos() {
+            return this.todos;
+        }
+    };
+}
